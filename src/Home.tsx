6bc56adbbd8085d2c18f6e28d59075ec82375e1f
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-08-29 13:32:06 +0300

    Print current date

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,12 +1,15 @@
 import Link from 'next/link';
 import React from 'react';
 
+const today = new Date();
+
 const Home: React.SFC = () => (
     <div>
     Hello World.{' '}
     <Link href="/about">
       <a>About</a>
     </Link>
+    <p>Tänään on {today.toLocaleDateString('fi')}</p>
   </div>
 )
 
