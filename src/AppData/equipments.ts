ea40c118ce988b82afe8cd2969202a79817ce21c
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-11-12 19:15:28 +0200

    Add fake data

---
+++ src/AppData/equipments.ts
@@ -0,0 +1,7 @@
+const equipments = [
+    { id: '001', name: 'Tablet', description: 'Very good tablet.' },
+    { id: '002', name: 'Phone', description: 'Not so good Android.' },
+    { id: '003', name: 'Phablet', description: 'Too expensive phablet.' },
+]    
+
+export default equipments;
---
+++ src/AppData/users.ts
@@ -0,0 +1,7 @@
+const users = [
+    { id: '001', name: 'Erkki', email: 'erkki123@example.com' },
+    { id: '002', name: 'Kari', email: 'kartsa1337@example.com' },
+    { id: '003', name: 'Esa', email: 'esa@esat.com' },
+]
+
+export default users;
