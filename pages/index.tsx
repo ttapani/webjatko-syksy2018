59b01eb1c62844a03dad51cc1b365ee723b31bf5
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-11-12 19:32:53 +0200

    Add index view

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Layout from '../src/Layout/Layout';
+import DashboardView from '../src/Views/DashboardView';
 
 const IndexPage: React.SFC<{}> = () => {
     return (
         <Layout title={"Dashboard"}>
-            <p>Welcome to Loan system.</p>
+            <DashboardView/>
         </Layout>
     );
 }
