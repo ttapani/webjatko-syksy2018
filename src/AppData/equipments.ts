3da547a3d6236b769be9de1ef79ff5de1337454d
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-11-16 19:10:33 +0200

    Add some data

--- src/AppData/equipments.ts
+++ src/AppData/equipments.ts
@@ -2,6 +2,8 @@ const equipments = [
     { id: '001', name: 'Tablet', description: 'Very good tablet.' },
     { id: '002', name: 'Phone', description: 'Not so good Android.' },
     { id: '003', name: 'Phablet', description: 'Too expensive phablet.' },
+    { id: '004', name: 'Laptop', description: 'Too expensive laptop.' },
+    { id: '005', name: 'Desktop', description: 'Very cheap phablet.' },
 ]    
 
 export default equipments;
--- src/AppData/loans.ts
+++ src/AppData/loans.ts
@@ -2,6 +2,7 @@ const loans = [
     { id: '001', equipmentId: '001', userId: '003', begins: '2018-09-09', ends: '2018-10-15', returned: null },
     { id: '002', equipmentId: '002', userId: '001', begins: '2018-08-01', ends: '2018-09-05', returned: '2018-09-04' },
     { id: '003', equipmentId: '001', userId: '002', begins: '2018-11-15', ends: '2018-12-06', returned: null },
+    { id: '004', equipmentId: '005', userId: '001', begins: '2018-11-15', ends: '2018-12-06', returned: null },
 ]
 
 export default loans;
