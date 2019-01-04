e76979a538319c726e97ca65d1c8abec35d1395d
Tomi Heino <tomi.heino@tuni.fi>
2019-01-04 05:21:38 +0200

    Add simple fakeapi and promises

--- src/Store/login/actions.ts
+++ src/Store/login/actions.ts
@@ -1,5 +1,5 @@
 import { ActionCreator } from 'redux';
-import { LoginAction, User, UserCredentials, LoginState } from './types';
+import { LoginAction, User, UserCredentials } from './types';
 import { ThunkAction, ThunkDispatch } from 'redux-thunk';
 
 export const loginUserStarted: ActionCreator<LoginAction> = (credentials: UserCredentials) => ({
@@ -25,14 +25,35 @@ export const loginUserFailure: ActionCreator<LoginAction> = (error: String) => (
 
 export const loginUser = (credentials: UserCredentials): ThunkAction<Promise<void>, {}, {}, LoginAction> => {
     return async (dispatch: ThunkDispatch<{}, {}, LoginAction>): Promise<void> => {
-        return new Promise<void>((resolve) => {
+        return new Promise<void>((resolve, reject) => {
             dispatch(loginUserStarted(credentials))
             console.log("login in progress");
             console.log(credentials);
             setTimeout(() => {
-                dispatch(loginUserSuccess({ userId: "asd", userName: "keke", type: 'normal' }))
-                resolve();
+                let response = fakelogin(credentials);
+                if(response.error == null) {
+                    dispatch(loginUserSuccess({ userId: response.userId, userName: response.userName, type: response.type }));
+                    resolve()
+                } else {
+                    dispatch(loginUserFailure(response.error));
+                    reject();
+                }
             }, 1000);
         });
     };
 };
+
+interface Response {
+    userId?: string;
+    userName?: string;
+    type?: string;
+    error?: string;
+}
+
+const fakelogin = (credentials: UserCredentials): Response => {
+    if(credentials.userName == "asd" && credentials.password == "asd") {
+        return { userId: "asd", userName: "keke", type: 'normal' };
+    } else {
+        return { error: "Invalid email or password" };
+    }
+}
