653a04b67b5fc6ddc5f9742fca0536f83edb3644
Tomi Heino <tomi.heino@tuni.fi>
2019-01-04 05:56:14 +0200

    Add user name on appbar, logout button

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import { createStyles, WithStyles, Theme } from '@material-ui/core';
+import { createStyles, WithStyles, Theme, Button } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
 import ToolBar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { AccountCircle } from '@material-ui/icons';
+import { ApplicationState } from 'src/Store/store';
+import { User, LoginState, LoginAction } from 'src/Store/login/types';
+import { ThunkDispatch } from 'redux-thunk';
+import { connect } from 'react-redux';
+import { logoutUser } from '../../src/Store/login/actions';
+import Router from 'next/router';
 
 
 interface IHeaderProps extends WithStyles<typeof styles> {
     title: string;
     handleMenuNuttonClick: () => void;
 }
+
+interface StateProps {
+    user: User;
+}
+
+interface DispatchProps {
+    logoutUser: () => Promise<void>;
+}
+
+type Props = StateProps & IHeaderProps & DispatchProps;
 
 const drawerWidth = 240;
 
@@ -36,12 +52,24 @@ const styles = (theme: Theme) => createStyles({
             display: 'none',
         },
     },
-
+    userButton: {
+        margin: theme.spacing.unit,
+    },
+    rightIcon: {
+        marginLeft: theme.spacing.unit,
+    }
 });
 
-class Header extends React.Component<IHeaderProps, null> {
-    constructor(props) {
+class Header extends React.Component<Props, null> {
+    constructor(props: Props) {
         super(props);
+        this.handleLogout = this.handleLogout.bind(this);
+    }
+
+    private handleLogout(event: React.MouseEvent<HTMLButtonElement>) {
+        this.props.logoutUser().then(() => {
+            Router.push("/login");
+        });
     }
     
     public render(): React.ReactNode {
@@ -60,15 +88,36 @@ class Header extends React.Component<IHeaderProps, null> {
                     <Typography variant="h6" color="inherit" noWrap={true} className={classes.title}>
                         {title}
                     </Typography>
-                    <Typography variant="subtitle1" color="inherit" noWrap={true}>
-                        Guest
-                    </Typography>
-                    <IconButton color="inherit">
-                        <AccountCircle />
-                    </IconButton>
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        className={classes.userButton}   
+                    >
+                        {this.props.user.userName}
+                        <AccountCircle className={classes.rightIcon}/>
+                    </Button>
+                    <Button
+                        color="inherit"
+                        className={classes.userButton}
+                        onClick={this.handleLogout}
+                    >
+                        Logout                        
+                    </Button>
                 </ToolBar>
             </AppBar>
         );
     }
 }
-export default withStyles(styles)(Header);
+
+const mapStateToProps = ({ login: { user, isLoading, error }}: ApplicationState): StateProps => ({ user });
+const mapDispatchToProps = (dispatch: ThunkDispatch<LoginState, undefined, LoginAction>, ownProps: IHeaderProps): DispatchProps => {
+    return {
+        logoutUser: async() => {
+            await dispatch(logoutUser())
+        }
+    }
+}
+
+export default connect<StateProps, DispatchProps, IHeaderProps>(mapStateToProps, mapDispatchToProps)(withStyles(styles)(Header));
+
+// export default withStyles(styles)(Header);
