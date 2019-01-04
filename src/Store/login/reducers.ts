7fb51b2b7fce61ce4be4f466c3f678396cf40834
Tomi Heino <tomi.heino@tuni.fi>
2019-01-04 05:55:42 +0200

    Add reducer cases for logout

--- src/Store/login/reducers.ts
+++ src/Store/login/reducers.ts
@@ -8,14 +8,20 @@ export const initialState: LoginState = {
     error: null,
 };
 
+export const guestUser = { userId: null, userName: null, type: 'guest' };
+
 const loanReducer: Reducer<LoginState> = (state: LoginState = initialState, action) => {
     switch ((action as LoginAction).type) {
         case 'login/LOGIN_STARTED':
-            return { ...state, user: { userId: null, userName: null, type: 'guest' }, isLoading: true, error: null };
+            return { ...state, user: guestUser, isLoading: true, error: null };
         case 'login/LOGIN_SUCCESS':
             return { ...state, user: action.payload.user, isLoading: false, error: null };
         case 'login/LOGIN_FAILURE':
             return { ...state, error: action.payload.error, isLoading: false };
+        case 'login/LOGOUT_STARTED':
+            return { ...state, isLoading: true };
+        case 'login/LOGOUT_SUCCESS':
+            return { ...state, user: guestUser, isLoading: false };
         default:
             return state;
     }
