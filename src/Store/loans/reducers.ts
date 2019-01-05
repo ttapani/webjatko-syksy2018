bd48db87d74f910750de0feec48cb95889f83a82
Tomi Heino <tomi.heino@tuni.fi>
2019-01-05 21:10:32 +0200

    Move loan data modification to redux

--- src/Store/loans/reducers.ts
+++ src/Store/loans/reducers.ts
@@ -1,6 +1,7 @@
 import { Reducer } from 'redux';
 import { LoanState, LoanAction } from './types';
 import loans from '../../AppData/loans';
+import { addItemsToData, updateItemsInData, removeItemsFromData } from '../Helpers/itemhelpers';
 
 export const initialState: LoanState = {
     loans: loans,
@@ -10,6 +11,12 @@ const loanReducer: Reducer<LoanState> = (state: LoanState = initialState, action
     switch ((action as LoanAction).type) {
         case 'loans/SET':
             return { ...state, loans: action.payload.loans };
+        case 'loans/ADD':
+            return { ...state, loans: addItemsToData(state.loans, action.payload.loans)};
+        case 'loans/UPDATE':
+            return { ...state, loans: updateItemsInData(state.loans, action.payload.loans)}
+        case 'loans/REMOVE':
+            return { ...state, loans: removeItemsFromData(state.loans, action.payload.loans)}
         default:
             return state;
     }
